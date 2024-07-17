4ba3fa6498001ed2ad23a6d22a911c420ce04bb1
Sylvia Blaho <sylvia.blaho@gmail.com>
2024-07-17 03:02:43 +0000

    add docstring

--- likes/serializers.py
+++ likes/serializers.py
@@ -1,11 +1,14 @@
 from rest_framework import serializers
-from .models import Like
+from likes.models import Like
+
 
 class LikeSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Like model
+    The create method handles the unique constraint on 'owner' and 'post'
+    """
     owner = serializers.ReadOnlyField(source='owner.username')
 
     class Meta:
         model = Like
-        fields = [
-            'id', 'owner', 'post', 'created_at'
-        ]
+        fields = ['id', 'created_at', 'owner', 'post']
