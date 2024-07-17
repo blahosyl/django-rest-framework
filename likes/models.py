dc8e94bad01de732c1312663ebe701ee9cb84080
Sylvia Blaho <sylvia.blaho@gmail.com>
2024-07-17 02:57:28 +0000

    add docstring

--- likes/models.py
+++ likes/models.py
@@ -3,19 +3,20 @@ from django.contrib.auth.models import User
 from posts.models import Post
 
 
-class Comment(models.Model):
+class Like(models.Model):
     """
-    Comment model, related to User and Post
+    Like model, related to 'owner' and 'post'.
+    'owner' is a User instance and 'post' is a Post instance.
+    'unique_together' makes sure a user can't like the same post twice.
     """
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(
-        Post, on_delete=models.CASCADE,
-        related_name='likes'
+        Post, related_name='likes', on_delete=models.CASCADE
     )
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        ordering = ['-created_at'],
+        ordering = ['-created_at']
         unique_together = ['owner', 'post']
 
     def __str__(self):
