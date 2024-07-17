e4dba13d50ff20470b5df7a6b961b7b31c292889
Sylvia Blaho <sylvia.blaho@gmail.com>
2024-07-17 15:28:51 +0000

    add new fields to ProfileDetail view

--- profiles/views.py
+++ profiles/views.py
@@ -32,4 +32,8 @@ class ProfileList(generics.ListAPIView):
 class ProfileDetail(generics.RetrieveUpdateAPIView):
     serializer_class = ProfileSerializer
     permission_classes = [IsOwnerOrReadOnly]
-    queryset = Profile.objects.all()
+    queryset = Profile.objects.annotate(
+        posts_count=Count('owner__post', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True)
+    ).order_by('-created_at')
