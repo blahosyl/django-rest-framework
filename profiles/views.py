8dd7da032b0f6f403763e187ecd9e766a085e663
Sylvia Blaho <sylvia.blaho@gmail.com>
2024-07-17 15:23:09 +0000

    add extra fields to the ProfileList view for filtering

--- profiles/views.py
+++ profiles/views.py
@@ -1,7 +1,8 @@
-from rest_framework import generics
+from django.db.models import Count
+from rest_framework import generics, filters
+from drf_api.permissions import IsOwnerOrReadOnly
 from .models import Profile
 from .serializers import ProfileSerializer
-from drf_api.permissions import IsOwnerOrReadOnly
 
 
 class ProfileList(generics.ListAPIView):
@@ -11,8 +12,11 @@ class ProfileList(generics.ListAPIView):
     """
 
     serializer_class = ProfileSerializer
-    queryset = Profile.objects.all()
-
+    queryset = Profile.objects.annotate(
+        posts_count=Count('owner__post', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True)
+    ).order_by('-created_at')
 
 class ProfileDetail(generics.RetrieveUpdateAPIView):
     serializer_class = ProfileSerializer
