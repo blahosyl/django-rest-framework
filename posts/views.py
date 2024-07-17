c9ecf7ea7ac047e0d17e9b416f8ea4d02cb6ebce
Sylvia Blaho <sylvia.blaho@gmail.com>
2024-07-17 15:41:07 +0000

    add new fields to PostDetail view

--- posts/views.py
+++ posts/views.py
@@ -38,4 +38,7 @@ class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     """
     serializer_class = PostSerializer
     permission_classes = [IsOwnerOrReadOnly]
-    queryset = Post.objects.all()
+    queryset = Post.objects.annotate(
+        comments_count=Count('comment', distinct=True),
+        likes_count=Count('likes', distinct=True),
+    ).order_by('-created_at')
