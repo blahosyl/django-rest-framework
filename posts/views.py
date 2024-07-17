94d3039ea9c680f05a87f84980e2ac559ce409cd
Sylvia Blaho <sylvia.blaho@gmail.com>
2024-07-17 21:36:17 +0000

    add filters to PostList

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,6 @@
 from django.db.models import Count
 from rest_framework import permissions, generics, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from drf_api.permissions import IsOwnerOrReadOnly
 from .models import Post
 from .serializers import PostSerializer
@@ -22,6 +23,16 @@ class PostList(generics.ListCreateAPIView):
     filter_backends = [
         filters.OrderingFilter,
         filters.SearchFilter,
+        DjangoFilterBackend,
+    ]
+
+    filterset_fields = [
+        # posts written by people who a given profile follows
+        'owner__followed__owner__profile',
+        # posts liked by a given profile
+        'likes__owner__profile',
+        # posts owned by a give profile
+        'owner__profile',
     ]
 
     search_fields = [
