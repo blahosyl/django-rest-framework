306ed00ab3a20bae84ea49654c282e8d2aa42828
Sylvia Blaho <sylvia.blaho@gmail.com>
2024-07-20 16:43:48 +0000

    add `corsheaders` to installed apps and middleware

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -92,6 +92,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'dj_rest_auth.registration',
+    'corsheaders',
     # self-written apps
     'comments',
     'followers',
@@ -103,6 +104,7 @@ INSTALLED_APPS = [
 SITE_ID = 1
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
