0dbade3ebb326cda0414e28c51c079f6c87114ec
Sylvia Blaho <sylvia.blaho@gmail.com>
2024-08-08 00:09:58 +0000

    allow image to be null

--- posts/models.py
+++ posts/models.py
@@ -31,7 +31,8 @@ class Post(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField(blank=True)
     image = models.ImageField(
-        upload_to='images/', default='../default_post_w2n3ng', blank=True
+        upload_to='images/', default='../default_post_w2n3ng', blank=True, 
+        null=True
     )
     image_filter = models.CharField(
         max_length=32, choices=image_filter_choices, default='normal'
