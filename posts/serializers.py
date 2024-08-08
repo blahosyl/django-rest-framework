d7e1ceaa7bb6d639b4df8b99da5a79d96439579f
Sylvia Blaho <sylvia.blaho@gmail.com>
2024-08-08 01:11:17 +0000

    validate image properties only if there is an image

--- posts/serializers.py
+++ posts/serializers.py
@@ -13,16 +13,20 @@ class PostSerializer(serializers.ModelSerializer):
 
 
     def validate_image(self, value):
-        if value.size > 2 * 1024 * 1024:
-            raise serializers.ValidationError('Image size larger than 2MB!')
-        if value.image.height > 4096:
-            raise serializers.ValidationError(
-                'Image height larger than 4096px!'
-            )
-        if value.image.width > 4096:
-            raise serializers.ValidationError(
-                'Image width larger than 4096px!'
-            )
+        """
+        Validate image properties only if there is an image
+        """
+        if value != None:
+            if value.size > 2 * 1024 * 1024:
+                raise serializers.ValidationError('Image size larger than 2MB!')
+            if value.image.height > 4096:
+                raise serializers.ValidationError(
+                    'Image height larger than 4096px!'
+                )
+            if value.image.width > 4096:
+                raise serializers.ValidationError(
+                    'Image width larger than 4096px!'
+                )
         return value
 
     def get_is_owner(self, obj):
